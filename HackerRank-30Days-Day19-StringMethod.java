bc6e1733c173c1bc13b8b5d5362c93e9c8953088
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-01-24 16:27:52 +0530

    Create HackerRank-30Days-Day19-StringMethod.java

---
+++ HackerRank-30Days-Day19-StringMethod.java
@@ -0,0 +1,25 @@
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        StringTokenizer tokens = new StringTokenizer(input,"![,?.\'@+] ");
+        int count = 0;
+        ArrayList<String> array =new ArrayList<String>();
+        while(tokens.hasMoreTokens()){
+            count++;
+            String token = tokens.nextToken();
+            //System.out.println(token);
+            array.add(token);
+        }
+        System.out.println(count);
+        for(String item : array){
+            System.out.println(item);
+        }
+    }
+}
