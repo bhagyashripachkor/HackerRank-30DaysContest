6fadac222ee3c035e830019bdc09cb9ee17a186d
Bhagyashri Pachkor <bhagyashrip547@gmail.com>
2016-01-29 22:27:53 +0530

    Rename HackerRank-30Days-Day27-TestingPart2.java to HackerRank-30Days-Day27-Testing2.java

---
+++ HackerRank-30Days-Day27-Testing2.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("5");
+        
+         //test case1
+         //System.out.println("4");
+         
+         System.out.println("4 3");
+         System.out.println("-1 5 0 1");
+        
+         System.out.println("5 2");
+         System.out.println("-1 0 8 1 -9");
+        
+        System.out.println("6 5");
+         System.out.println("-1 0 4 -3 7 5");
+        
+        System.out.println("7 1");
+         System.out.println("-1 8 4 0 -6 -3 2");
+        
+        System.out.println("8 5");
+         System.out.println("0 -1 4 9 -2 6 3 7");
+            
+        
+    }
+}
